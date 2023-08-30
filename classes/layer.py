b38f7bd87a60f093cf680db67d9765b9f89f869b
Hardik Sharma <hardiksharmah73@gmail.com>
2023-08-30 17:07:55 +0530

    Created Neuron class

---
+++ classes/layer.py
@@ -0,0 +1,9 @@
+'''
+    This is the class that deals with the arrangement
+    of Neurons in a particular layer.
+'''
+
+class Layer:
+    def __init__(self, layerIndex, size = DEFAULT_LAYER_SIZE ):
+        self.layerIndex = layerIndex
+        self.
---
+++ classes/neuron.py
@@ -0,0 +1,32 @@
+'''
+    This is the primitive class `Neuron`, 
+    which is essentially the basic building
+    block of the Neural Network.
+'''
+
+class Neuron:
+    def __init__( self, classIndex , domain = [0,1] ):
+        self.layer = -1
+        self.value = 0
+        self.index = classIndex
+        self.bias = 0
+
+    def rectify( self , newValue ):
+        try :
+            if ( newValue < domain[0] or newValue > domain[1] ):
+                raise Exception
+            self.value = newValue
+        except :
+            print("Incorrect value passed.")
+    
+    def getLayer(self):
+        return self.layer
+
+    def getClassIndex(self):
+        return self.index
+
+    def getValue(self):
+        return self.value
+
+    def changeBias(self, newBias):
+        self.bias = newBias
