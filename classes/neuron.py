6b45fb32fd48ccd6f81a428cfd9a434000965b84
Hardik Sharma <hardiksharmah73@gmail.com>
2023-09-17 05:21:34 +0530

    completed the basic structure for neuron.py

--- classes/neuron.py
+++ classes/neuron.py
@@ -1,42 +1,58 @@
+from logs.logger import Log
+
 '''
     This is the primitive class `Neuron`, 
     which is essentially the basic building
     block of the Neural Network.
 '''
-
 class Neuron:
     '''
         This is the base Neuron class.
         
+        :param layer: Index of the layer this neuron belongs to.
         :param layerIndex: Index of the layer this neuron belongs to.
-        :param domain: Domain of the neuron values
+        :param domain: Domain of the neuron values.
+        :param activation: Default activation of the neuron.
+        :param bias: Default bias of the neuron.
     '''
-    def __init__( self, layerIndex , domain = [0,1] ):
-        self.layer = -1
-        self.value = 0
+    def __init__( self, layer : int, layerIndex : int, domain = [0,1], activation = 0.0, bias = 0.0 ):
+        self.layer = layer
+        self.activation = activation
         self.index = layerIndex
-        self.bias = 0
+        self.bias = bias
         self.domain = domain
+
+        self.logger = Log()
     
-    '''
-        Called after running the Backpropagation algorithm.
-    '''
-    def rectify( self , newValue ):
+    def setActivation(self, newActivation):
         try :
-            if ( newValue < self.domain[0] or newValue > self.domain[1] ):
-                raise Exception
-            self.value = newValue
-        except :
-            print("Incorrect value passed.")
+            if ( newActivation < self.domain[0] or newActivation > self.domain[1] ):
+                self.activation = newActivation
+            else :
+                self.logger.logWarning("Activation value is out of range. No change made.")
+        except Exception as E :
+            self.logger.logException(message=str(E))
     
+    '''
+        Updates the bias of the Neuron.
+    '''
+    def updateBias(self, newBias):
+        self.bias = newBias
+    
+    '''
+        Returns the index of the layer
+    '''
     def getLayer(self):
         return self.layer
-
-    def getClassIndex(self):
+    
+    '''
+        Returns the index of the neuron within the layer.
+    '''
+    def getIndex(self):
         return self.index
-
-    def getValue(self):
-        return self.value
-
-    def changeBias(self, newBias):
-        self.bias = newBias
+    
+    '''
+        Returns the activation of the neuron.
+    '''
+    def getActivation(self):
+        return self.activation
