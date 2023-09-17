5699738a374fb68d0c131eb256e2faf85fe69aad
Hardik Sharma <hardiksharmah73@gmail.com>
2023-09-17 06:09:56 +0530

    Added a few more properties to the NeuralNetwork class.

--- classes/network.py
+++ classes/network.py
@@ -2,28 +2,50 @@
     The overall structure of the neural network.
 '''
 from logs.logger import Logger
+import numpy
 
 class NeuralNetwork:
-    def __init__(self, inputFeautures : int , outputPossibilities : int, numHiddenLayers = 2, neuronVector = [16,16], domain = [0,1]):
-        self.inputFeautures = inputFeautures
-        self.outputPossibilities = outputPossibilities
+    def __init__(self, numInputFeautures : int , numOutputFeatures : int, 
+                 numHiddenLayers = 2, neuronVector = [16,16], domain = [0,1],
+                 intermediateActivationFunction = 'LeakyRelu', endActivationFunction = 'softmax',
+                 backpropagationAlgorithm = 'SGD', lossFunction = 'crossEntropy'):
+
+        self.numInputFeautures = numInputFeautures
+        self.inputFeautures = numpy.array(numInputFeautures)
+
+        self.numOutputFeatures = numOutputFeatures
+        self.outputFeatures = numpy.array(numOutputFeatures)
+        
         self.numHiddenLayers = numHiddenLayers
         self.neuronVector = neuronVector
+
         self.domain = domain
+        self.intermediateActivationFunction = intermediateActivationFunction
+        self.endActivationFunction = endActivationFunction
+        self.backpropagationAlgorithm = backpropagationAlgorithm
+        self.lossFunction = lossFunction
 
         self.logger = Logger()
 
+    
+    def feedInputFeatures(self, inputFeautures):
+        self.inputFeautures = inputFeautures
 
     def train(self, targetEpochs = 50, hyperParameters : dict = {}, displayIntermediateResults : bool = False):
-        for epoch in range(targetEpochs):
-            self.__computeForwardPropagation()
-            self.__computeLossFunction()
-            self.__updateModelParameters(epoch, hyperParameters=hyperParameters)
-            
-            if ( displayIntermediateResults ):
-                self.__displayModelResults()
+        try :
+            for epoch in range(targetEpochs):
+                self.__computeForwardPropagation()
+                self.__computeLossFunction()
+                self.__updateModelParameters(epoch, hyperParameters=hyperParameters)
+                
+                if ( displayIntermediateResults ):
+                    self.__displayModelResults()
 
-    def __computeForwardPropagation(self):
+                self.logger.logInfo(f"Epoch {epoch + 1} completed. Model performace : {self.__getModelPerformance}")
+        except Exception as E :
+            self.logger.logException(message=str(E))
+    
+    def __computeForwardPropagation(self, batchSize = 10):
         pass
 
     def __computeLossFunction(self):
@@ -33,4 +55,7 @@ class NeuralNetwork:
         pass
 
     def __displayModelResults(self):
+        pass
+
+    def __getModelPerformance(self):
         pass
